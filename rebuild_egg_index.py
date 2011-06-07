342cc352b5ac54256ab480441c97294f7fe0ee64
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-06-07 11:03:00 -0400

    update index and builder script

--- rebuild_egg_index.py
+++ rebuild_egg_index.py
@@ -1,12 +1,36 @@
 import os
+import time
+
+def convert_bytes(bytes):
+    bytes = float(bytes)
+    if bytes >= 1099511627776:
+        terabytes = bytes / 1099511627776
+        size = '%.2fT' % terabytes
+    elif bytes >= 1073741824:
+        gigabytes = bytes / 1073741824
+        size = '%.2fG' % gigabytes
+    elif bytes >= 1048576:
+        megabytes = bytes / 1048576
+        size = '%.2fM' % megabytes
+    elif bytes >= 1024:
+        kilobytes = bytes / 1024
+        size = '%.2fK' % kilobytes
+    else:
+        size = '%.2fb' % bytes
+    return size
 
 fp = open("eggs/index.html", 'w')
 
-print >> fp, "<html><body><h1>Index of dist.socialplanning.org/eggs</h1><ul>"
+print >> fp, "<html><body><h1>Index of dist.socialplanning.org/eggs</h1><table><tr><th>Filename</th><th>Size</th><th>Last Modified</th></tr>"
 
 for filename in sorted(os.listdir("eggs")):
-    print >> fp, """<li><a href="/eggs/%s">%s</a></li>""" % (
+    print >> fp, """<tr><td><a href="/eggs/%s">%s</a></td>""" % (
         filename, filename)
-
-print >> fp, "</ul></body></html>"
+    stat = os.stat("eggs/" + filename)
+    print >> fp, """<td>%s</td><td>%s</td></tr>""" % (
+        convert_bytes(stat.st_size),
+        time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(stat.st_mtime)),
+        )
+        
+print >> fp, "</table></body></html>"
 fp.close()
